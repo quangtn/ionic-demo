901a0488e1745dfb0e461e2c19754c65f8d79bf1
Andrew Joslin <andytjoslin@gmail.com>
2014-05-27 11:38:06 -0600

    remove bad folder

--- ionSideMenus/simple/index.js
+++
@@ -1,9 +0,0 @@
-
-var app = angular.module('simple', ['ionic']);
-app.controller('SideMenusSimpleCtrl', function($scope, $ionicSideMenuDelegate) {
-
-  $scope.toggleLeft = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
-
-});
