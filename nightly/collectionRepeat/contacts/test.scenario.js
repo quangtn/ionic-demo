7c63e8958ba3647a1a480c0991370f272385b6d3
Ionitron <hi@ionicframework.com>
2014-06-05 20:14:59 +0000

    demos: update for nightly

--- nightly/collectionRepeat/contacts/test.scenario.js
+++ nightly/collectionRepeat/contacts/test.scenario.js
@@ -5,17 +5,6 @@ it('should init', function() {
 });
 
 
-it('should scroll to the bottom', function(){
-  var ele = element(by.css('.bar-header .button'));
-  ele.click();
-  browser.sleep(200);
-});
-
-it('should scroll to the top', function(){
-  var ele = element(by.css('.bar-header'));
-  ele.click();
-  browser.sleep(200);
-});
 
 it('should filter by juan', function(){
   var ele = element(by.model('search'));
